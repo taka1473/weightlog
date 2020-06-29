e724c89f4cf0430de6267a9538ee427513746b07
taka.s <takapie81@gmail.com>
2020-06-29 05:26:18 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'toppages#top'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only:[:show, :new, :create]
 end
